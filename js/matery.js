286cc430336a37a2a7fa770908b97369505a695e
yinglonglll <948706806@qq.com>
2021-12-04 00:12:35 +0800

    Site updated: 2021-12-04 00:12:33

--- js/matery.js
+++ js/matery.js
@@ -1,4 +1,6 @@
 $(function () {
+
+    
     /**
      * 添加文章卡片hover效果.
      */
@@ -170,3 +172,20 @@ $(function () {
     // 初始化加载 tooltipped.
     $('.tooltipped').tooltip();
 });
+
+// 深色模式设置 
+function switchNightMode() { 
+    var body = document.body; 
+    if(body.classList.contains('dark')){ 
+    document.body.classList.remove('dark'); 
+    localStorage.setItem('dark','0'); 
+    $('#nightMode').removeClass("fa-lightbulb-o").addClass("fa-moon-o"); 
+    return; 
+    } 
+    else { 
+    document.body.classList.add('dark'); 
+    localStorage.setItem('dark','1'); 
+    $('#nightMode').removeClass("fa-moon-o").addClass("fa-lightbulb-o"); 
+    return; 
+    } 
+}
